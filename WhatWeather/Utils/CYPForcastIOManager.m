e922fefe793205173abac6bf08760c5bdd92bc39
Yongping Chen <will@nowhere.co.jp>
2014-06-21 15:14:00 +0900

    finish the method to request forcast data

--- WhatWeather/Utils/CYPForcastIOManager.m
+++ WhatWeather/Utils/CYPForcastIOManager.m
@@ -7,11 +7,12 @@
 //
 
 #import "CYPForcastIOManager.h"
+#import <AFNetworking/AFNetworking.h>
+
 @interface CYPForcastIOManager()
-{
-    NSString *apiKey;
-    
-}
+
+@property (nonatomic, retain) NSString *apiKey;
+@property (nonatomic, retain) NSString *forcastBasicURL;
 @end
 
 @implementation CYPForcastIOManager
@@ -23,10 +24,23 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedManager = [[self alloc] init];
-        
+        sharedManager.apiKey = @"e4e16728cc706d98e06f30e831e20467";
+        sharedManager.forcastBasicURL = [NSString stringWithFormat:@"%@/%@", @"https://api.forecast.io/forecast", sharedManager.apiKey];
     });
     
     return sharedManager;
+}
+
+-(void)forcastRequestWithLongitude:(NSNumber *)longitude latitude:(NSNumber *)latitude FinishedBlock:(CYPForcastIORequestFinishBlock)finishedBlock failedBlock:(CYPForcastIORequestFailedBlock)failedBlock
+{
+    NSString *requestURLString = [NSString stringWithFormat:@"%@/%f,%f",_forcastBasicURL,latitude.floatValue, longitude.floatValue];
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:requestURLString parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        finishedBlock(responseObject);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failedBlock(error);
+    }];
 }
 
 
