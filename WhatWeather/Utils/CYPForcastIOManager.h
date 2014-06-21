298c1eefe571801e8ae433ce1c5118096511f52d
Yongping Chen <will@nowhere.co.jp>
2014-06-21 14:35:49 +0900

    create CYPForcastIOManager

---
+++ WhatWeather/Utils/CYPForcastIOManager.h
@@ -0,0 +1,13 @@
+//
+//  CYPForcastIOManager.h
+//  WhatWeather
+//
+//  Created by Chen Yongping on 6/21/14.
+//  Copyright (c) 2014 AllRoudHut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CYPForcastIOManager : NSObject
+
+@end
---
+++ WhatWeather/Utils/CYPForcastIOManager.m
@@ -0,0 +1,13 @@
+//
+//  CYPForcastIOManager.m
+//  WhatWeather
+//
+//  Created by Chen Yongping on 6/21/14.
+//  Copyright (c) 2014 AllRoudHut. All rights reserved.
+//
+
+#import "CYPForcastIOManager.h"
+
+@implementation CYPForcastIOManager
+
+@end
